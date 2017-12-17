3e974c3d0c839c76b299582f577193b25f2059ff
Unknown <nimeshika94@gmail.com>
2017-12-17 21:17:55 +0530

    set content in homepage

--- application/views/index.php
+++ application/views/index.php
@@ -112,33 +112,38 @@
     </section>
 </header>
 <div class="block-1">
-    <div class="container">
-        <div class="row">
-            <div class="grid_3">
-                <div class="block-1_count">10</div>
-                Years of <br> Experience
-                <div class="clear"></div>
-            </div>
-            <div class="grid_3">
-                <div class="block-1_count">30</div>
-                Successful <br> Projects
-                <div class="clear"></div>
-            </div>
-            <div class="grid_3">
-                <div class="block-1_count">20</div>
-                Partner <br> Projects
-                <div class="clear"></div>
-            </div>
-            <div class="grid_3">
-                <a href="#" class="support"><img src="images/support.png" alt=""></a>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="grid_3">
+        <div class="block-1_count">1</div>
+        Year of <br> Experience
+        <div class="clear"></div>
+      </div>
+      <div class="grid_3">
+        <div class="block-1_count"></div>
+        Computer Hardware <br> Sellers
+        <div class="clear"></div>
+      </div>
+      <div class="grid_3">
+        <div class="block-1_count"></div>
+        CCTV Camera <br> Fitting
+        <div class="clear"></div>
+      </div>
+      <div class="grid_3">
+        <div class="block-1_count"></div>
+        Trusted Service<br> Crew
+        <div class="clear"></div>
+      </div>
+      <div class="grid_3">
+        <a href="#" class="support"><img src="images/support.png" alt=""></a>
+      </div>
     </div>
+  </div>
 </div>
 <!--=====================
           Content
 ======================-->
-<section id="content"><div class="ic">More Website Templates @ TemplateMonster.com - July 28, 2014!</div>
+<section id="content"><div class="ic"></div>
     <div class="container">
         <div class="row">
             <div class="grid_10 preffix_1 ta__center">
@@ -146,8 +151,7 @@
                     <h2 class="head__1">
                         Welcome
                     </h2>
-                    <p>Find the perfect accessory for your computer including external hard drives, routers, keyboards, mice, PC headsets, gaming products, Amazon's Choice routers, and Renewed Accessories. </p>
-                    Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet site amet est vel, commodo venenatis eros. Fusce adipiscing quam id risus sagittis, non consequat lacus interdum. nulla, eu sodales arcu. Quisque viverra nulla nunc, eu ultrices libero ultricies eget. Phasellus accumsan
+                    <p>WebStation Technologies is the newly established company in Battaramulla. Mainly we are focusing on selling and renting Computer Hardware, PCs and Laptops. At the same time we are working on fitting CCTV cameras. Our official website will be helpful to you to find your needs and services. There is more friendly and active service crew with us. So welcome you all to visit our site and make your dreams true. </p>
                 </div>
             </div>
         </div>
@@ -161,31 +165,24 @@
                         <img src="<?php echo base_url();?>assets/images/page1_img1.jpg" alt="" class="img_inner fleft">
                         <div class="extra_wrapper">
                             <div class="text1"><a href="#">Experience</a></div>
-                            <p>Curabitur vel lorem sit amet nulla ullamcorper fermentum In vitae dert varius augue, eu consectetur ligulaam dui eros dertoli.</p>Fusce adipiscing quam id risus sagittis, non consequat lacus interdum. Proin ut tinciduntol nulla, eu sodales vertomin
+                            <p>We have one year experience and we can proudly said that we gave our service to Kandy Texile (Pvt) Ltd branches in Kadawatha, Kiribathgoda and Wallawatte.</p>
                             <br>
-                            <a href="#" class="link-1">more</a>
                         </div>
                     </div>
                     <div class="block-2">
                         <img src="<?php echo base_url();?>assets/images/page1_img2.jpg" alt="" class="img_inner fleft">
                         <div class="extra_wrapper">
                             <div class="text1"><a href="#">Marketing</a></div>
-                            <p>Curabitur vel lorem sit amet nulla ullamcorper fermentum In vitae dert varius augue, eu consectetur ligulaam dui eros dertoli.</p>Fusce adipiscing quam id risus sagittis, non consequat lacus interdum. Proin ut tinciduntol nulla, eu sodales vertomin
+                            <p>We assemble Desktop PCs / Branded PCs , all kind of Notebooks , Computer Accessories and Network Products</p>
                             <br>
-                            <a href="#" class="link-1">more</a>
                         </div>
                     </div>
                 </div>
                 <div class="grid_4 preffix_1">
-                    <h3>Testimonials</h3>
-                    <blockquote class="bq1">
-                        <p>“Curabitur vel lorem sit amet nulla erero fermentum. In vitae varius auguectetu ligula. Etiam dui eros, laoreet site am est vel commodo venenatisipiscing... ”</p>
-                        <span>Liza Jons</span>
-                    </blockquote>
-                    <blockquote class="bq1">
-                        <p>“Burabitur vel lorem sit amet nulla erero fermentum. In vitae varius auguectetu ligula. Etiam dui eros, laoreet site am ast vel commodo venenatisipiscino... ”</p>
-                        <span>Mark Brown</span>
-                    </blockquote>
+                    <h3></h3>
+                    <p><h4>
+                        We are anxious to build-up business relations with your esteemed organization and would be much thankful if you could consider our company on the services. Our well trained and technically qualified staff focus on your need and act promptly to the entire satisfaction.
+                    </h4></p>
                 </div>
             </div>
         </div>
@@ -195,29 +192,29 @@
             <div class="grid_5">
                 <h4>About Company</h4>
                 <img src="<?php echo base_url();?>assets/images/page1_img3.jpg" alt="" class="img_inner fleft">
-                <p>Curabitur vel lorem sit amet nulla ullamcorper fermentum In vitae dert arius augue, eu consectetur </p>
-                <p class="offset__1">Eligulaam dui eros dertolisce dertolo adipiscing quam id risus sagittis</p>
-                Curabitur vel lorem sit amet nulla ullamcorper fermentum In vitae dert rius augue, eu consectetur larem dui eros dertolisce dertolo
+                <p>WebStation Technologies is the newly established company in Battaramulla.</p>
+                <p class="offset__1">Mainly we are focusing on selling and renting Computer Hardware, PCs and Laptops.</p>
+                <br>
+                At the same time we are working on fitting CCTV cameras and assembling computer accessories and network products.
             </div>
             <div class="grid_4">
                 <h4>Solutions</h4>
                 <ul class="list-1">
-                    <li><a href="#">Vivamus at magna non nunc tristique </a></li>
-                    <li><a href="#">Aliquam nibh ante, egestas id</a></li>
-                    <li><a href="#">Ommodo luctus libero</a></li>
-                    <li><a href="#">Faucibus malesuada faucibusonec </a></li>
-                    <li><a href="#">Laoreet metus id laoreet</a></li>
-                    <li><a href="#">Jalesuadaorem ipsum dolor sit ame</a></li>
+                    <li>Computers Service and Maintenance </li>
+                    <li>Server Repair and Maintenance</li>
+                    <li>CCTV Camera Configuration and Installation</li>
+                    <li>Door Lock Systems Configuration and Installation</li>
+                    <li>Security Alarm System Configuration and Installation</li>
+                    <li>GPS Trackers Configuration and Installation</li>
                 </ul>
             </div>
             <div class="grid_3">
                 <h4>Contact Info</h4>
                 <address>
                     <ul class="cont_address">
-                        <li>8901 Marmora Road, Glasgow, D04 89GR</li>
-                        <li>+1 (800) 123 9876</li>
-                        <li><a href="#">info@demolink.org</a></li>
-                        <li><a href="#">www.demolink.org</a></li>
+                        <li>No:30, Wickramasighepura, Battaramulla</li>
+                        <li>+94 77 723 9580/ +94 77 555 0223</li>
+                        <li>Fax : 011 208 209 7</a></li>
                     </ul>
                 </address>
             </div>
