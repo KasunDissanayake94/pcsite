b589fc4fe89bc9ba7ba703744a0b5895ead6cd10
Unknown <nimeshika94@gmail.com>
2017-12-18 11:05:16 +0530

    shopping cart changes

--- application/views/index.php
+++ application/views/index.php
@@ -44,7 +44,7 @@
                     </a>
                 </h1>
                 <ul class="sf-menu">
-                    <li style="float: right;height: 70px;width: 70px;margin-left: 20px"><a href="<?php echo base_url();?>index.php/auth/cart"><img src="<?php echo base_url();?>assets/images/shopping_cart1.png" alt="Logo alt"></a></li>
+                    <li style="float: right;height: 50px;width: 50px;margin-left: 20px"><a href="<?php echo base_url();?>index.php/auth/cart"><img src="<?php echo base_url();?>assets/images/shopping_cart1.png" alt="Logo alt"></a></li>
 
                     <li style="float: right;margin-left: 20px"><a href="<?php echo base_url();?>index.php/auth/login"> Login </a></li>
 
